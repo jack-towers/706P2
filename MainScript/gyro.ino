89f3fa4bd1e57381b7b7064165f8f043feae6e8c
jack-towers <109499019+jack-towers@users.noreply.github.com>
2024-04-23 08:38:05 +1200

    Wow

--- MainScript/gyro.ino
+++ MainScript/gyro.ino
@@ -29,33 +29,3 @@ float gyroRead() {
 
   return currentAngle;
 }
-
-float irRead() {
-
-  int previous_front_right = 0;
-  int previous_back_right = 0;
-  int previous_front_left = 0;
-  int previous_back_left = 0;
-
-  
-  current_front_right = 2227.4 * pow(analogRead(IR_front_right), -0.981);
-  current_back_right = 62843.2 * pow(analogRead(IR_back_right), -1.111);
-  current_front_left = 6336.7 * pow(analogRead(IR_front_left), -1.065);
-  current_back_left = 5259.9 * pow(analogRead(IR_back_left), -0.978);
-
-  if (abs(current_front_right - previous_front_right) < 5) {
-    previousIR_cm = current_front_right;
-  } 
-
-  if (abs(current_back_right - previous_back_right) < 5) {
-    previousIR_cm = current_back_right;
-  } 
-
-  if (abs(current_front_left - previous_front_left) < 5) {
-    previousIR_cm = current_front_left;
-  } 
-
-  if (abs(current_back_left - previous_back_left) < 5) {
-    previousIR_cm = current_back_left;
-  } 
-}
