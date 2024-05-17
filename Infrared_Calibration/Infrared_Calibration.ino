54949686be7a5637f70b4522bbe45f6473a64f6d
chiyanguoa <109586069+chiyanguoa@users.noreply.github.com>
2024-05-17 14:35:14 +1200

    Update Infrared_Calibration.ino

--- Infrared_Calibration/Infrared_Calibration.ino
+++ Infrared_Calibration/Infrared_Calibration.ino
@@ -1,28 +1,60 @@
-int IR = A9; //sensor is attached on pinA0
-int C = A11;
+#include <SoftwareSerial.h>
 
+// Serial Data input pin
+#define BLUETOOTH_RX 10
+// Serial Data output pin
+#define BLUETOOTH_TX 11
+
+SoftwareSerial BluetoothSerial(BLUETOOTH_RX, BLUETOOTH_TX);
+
+//Front Right
+//ir_A_LONG
+int IR_front_right = A10;
+
+//Front Left
+//ir_C_Long
+int IR_front_left = A8;
+
+//Side Right
+//ir_W_Short
+int IR_side_right = A11;
+
+//Side left
+//ir_Y_Short
+int IR_side_left = A9;
 
 byte serialRead = 0; //for control serial communication
-int signalADC = 0; // the read out signal in 0-1023 corresponding to 0-5v
+int signalADC1 = 0; // the read out signal in 0-1023 corresponding to 0-5v
+int signalADC2 = 0; // the read out signal in 0-1023 corresponding to 0-5v
+int signalADC3 = 0; // the read out signal in 0-1023 corresponding to 0-5v
+int signalADC4 = 0; // the read out signal in 0-1023 corresponding to 0-5v
+
 
 void setup() {
 // put your setup code here, to run once:
-BluetoothSerial.begin(115200);
-Serial.begin(9600); // start serial communication
+  BluetoothSerial.begin(115200);
+  Serial.begin(9600); // start serial communication
 }
 
 void loop() {
 // put your main code here, to run repeatedly:
-if (Serial.available()) // Check for input from terminal
-{
-  // signalADC = analogRead(W); // the read out is a signal from 0-1023 corresponding to 0-5v
-  // signalADC = 6336.7*pow(analogRead(C),-1.065 );
-  // Serial.print("C: ");
-  // Serial.println(signalADC);
+  if (Serial.available()) { // Check for input from terminal 
+    signalADC1 = analogRead(IR_front_left); // the read out is a signal from 0-1023 corresponding to 0-5v
+    Serial.print("FL: ");
+    Serial.println(signalADC1);
+    
+    signalADC2 = analogRead(IR_front_right); // the read out is a signal from 0-1023 corresponding to 0-5v
+    Serial.print("FR: ");
+    Serial.println(signalADC2);
 
-  signalADC = analogRead(IR); // the read out is a signal from 0-1023 corresponding to 0-5v
-  Serial.print("Y: ");
-  Serial.println(signalADC);
+    signalADC3 = analogRead(IR_side_left); // the read out is a signal from 0-1023 corresponding to 0-5v
+    Serial.print("SL: ");
+    Serial.println(signalADC3);
 
-  delay(2000);
+    signalADC4 = analogRead(IR_side_right); // the read out is a signal from 0-1023 corresponding to 0-5v
+    Serial.print("SR: ");
+    Serial.println(signalADC4);
+
+    delay(500);
+  }
 }
