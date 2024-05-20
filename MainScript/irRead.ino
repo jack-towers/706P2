518fa5d88805a5cd897e66ee21b6bbf75ffe41f7
Cole Parker <coleashtonparker@gmail.com>
2024-05-21 08:43:36 +1200

    h

--- MainScript/irRead.ino
+++ MainScript/irRead.ino
@@ -4,7 +4,7 @@ int IR_front_right = A10;
 
 //Back Right
 //ir_Y_short
-int IR_back_right = A11;
+int IR_side_right = A11;
 
 //Front Left
 //ir_A_Long
@@ -12,7 +12,7 @@ int IR_front_left = A8;
 
 //Back left
 //ir_B_Long
-int IR_back_left = A9;
+int IR_side_left = A9;
 
 
 float irRead() {
@@ -24,10 +24,10 @@ float irRead() {
   int previousIR_cm = 0;
 
   
-  int current_front_right = 2227.4 * pow(analogRead(IR_front_right), -0.981);
-  int current_back_right = 62843.2 * pow(analogRead(IR_back_right), -1.111);
-  int current_front_left = 6336.7 * pow(analogRead(IR_front_left), -1.065);
-  int current_back_left = 5259.9 * pow(analogRead(IR_back_left), -0.978);
+  int current_front_right = 7225.6 * pow(analogRead(IR_front_right), -1.081);//y = 72225.6x^-1.081
+  int current_side_right = 1861.7 * pow(analogRead(IR_back_right), -1.0.963);//y = 1861.7x^-0.963
+  int current_front_left = 48776 * pow(analogRead(IR_front_left), -1.395);//y = 48776x^-1.395
+  int current_side_left = 2328 * pow(analogRead(IR_back_left), -1);//y = 2328x^-1
 
   if (abs(current_front_right - previous_front_right) < 5) {
     previousIR_cm = current_front_right;
